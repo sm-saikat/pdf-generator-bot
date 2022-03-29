1e4e3f5a38d6fc31898525d2eb6678eee6d38274
sm-saikat <mail.smsaikat@gmail.com>
2022-03-29 17:43:07 +0600

    ready to deploy

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const webhookControllers = require('./controllers/webhookControllers')
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`);
+})
+
+app.get('/', (req, res)=>{
+    res.send('Homepage');
+})
+
+app.get('/webhook', webhookControllers.webhookGet)
+app.post('/webhook', webhookControllers.webhookPost)
